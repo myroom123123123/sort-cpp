6bb5a6ccaeccaaedb9370ed047325b7ddda57cfd
Petrenko Valeriya <mroom9195@gmail.com>
2024-05-27 19:00:20 +0300

    ex4

--- main.cpp
+++ main.cpp
@@ -1,26 +1,14 @@
 #include <iostream>
 using namespace std;
 
-bool isPrime(int n) {
-	if (n <= 1) {
-		return false;
-	}
-	for (int i = 2; i < n; i++) {
-		if (n % i == 0) {
-			return false;
-		}
-	}
-	return true;
+int cube(int num) {
+	return num * num * num;
 }
 
 int main() {
-	int n;
-	cout << "Enter the number: ";
-	cin >> n;
-	if (isPrime(n)) {
-		cout << n << " is a prime number." << endl;
-	}
-	else {
-		cout << n << " is not a prime number." << endl;
-	}
+	int num;
+	cout << "Enter a number: ";
+	cin >> num;
+	int result = cube(num);
+	cout << "The cube of " << num << " is " << result << endl;
 }
