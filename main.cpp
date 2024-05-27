aac0841d7dce370cd20f25336a566fdec1eafcd1
Petrenko Valeriya <mroom9195@gmail.com>
2024-05-27 19:17:59 +0300

    ex3

--- main.cpp
+++ main.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int sumRange(int a, int b) {
-	int sum = 0;
-	for (int i = a; i <= b; i++) {
-		sum += i;
-	}
-	return sum;
+bool isPerfect(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    int sum = 1;  
+    for (int i = 2; i <= n / 2; ++i) {
+        if (n % i == 0) {
+            sum += i;
+        }
+    }
+    return sum == n;
+}
+
+void findPerfectNumbers(int start, int end) {
+    cout << "Perfect numbers in the interval [" << start << ", " << end << "] are: ";
+    bool found = false;
+    for (int i = start; i <= end; ++i) {
+        if (isPerfect(i)) {
+            cout << i << " ";
+            found = true;
+        }
+    }
+    if (!found) {
+        cout << "None";
+    }
+    cout << endl;
 }
 
 int main() {
-	int a, b;
-	cout << "Enter the first number: ";
-	cin >> a;
-	cout << "Enter the second number: ";
-	cin >> b;
-	cout << "The sum of numbers in the range between " << a << " and " << b << " is " << sumRange(a, b) << endl;
-	return 0;
+    int start, end;
+    cout << "Enter the start of the interval: ";
+    cin >> start;
+    cout << "Enter the end of the interval: ";
+    cin >> end;
+    findPerfectNumbers(start, end);
+    return 0;
 }
